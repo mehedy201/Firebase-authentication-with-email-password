b18b1a168e532eb1eeb3d9f905dbd404739220c4
mehedy201 <mehedy643@gmail.com>
2022-04-08 14:11:56 +0600

    added some css

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
-import { getAuth } from "firebase/auth";
-import './App.css';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { useState } from "react";
+import "./App.css";
 import app from "./firebase.init";
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleEmail = (event) => {
-    console.log(event.target.value)
-  }
+    setEmail(event.target.value);
+  };
   // handle password
-  const handlePassword = event =>{
-    console.log(event.target.value)
-  }
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
   // form submit handle
-  const handleSubmitedForm = event => {
-    console.log('form submited')
-    event.preventDefault()
-  }
-
+  const handleSubmitedForm = (event) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setEmail("");
+        setPassword("");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    event.preventDefault();
+  };
 
   return (
     <div className="App">
-      <h1>Firebase authentication with email and password</h1>
-      <form onSubmit={handleSubmitedForm}>
-        <input onBlur={handleEmail} type="email" name="" id="" />
-        <input onBlur={handlePassword} type="password" name="" id="" />
-        <input type="submit" value="Login" />
-      </form>
+      <h1 className="title">Firebase authentication with Email and Password</h1>
+      <div className="form_box">
+        <h3>Please Resisterd!!!</h3>
+        <form onSubmit={handleSubmitedForm}>
+          <input className="input_field" onBlur={handleEmail} type="email" name="" id="" placeholder="Your Email" />
+          <input className="input_field" onBlur={handlePassword} type="password" name="" id="" placeholder="Password"/>
+          <input className="submit-btn" type="submit" value="Login"  />
+        </form>
+      </div>
     </div>
   );
 }
